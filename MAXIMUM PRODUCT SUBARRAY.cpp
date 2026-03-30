aef937107e71c54a0c1ee576d9f2156069bcdb6b
Sunidhi Chaturvedi <chaturvedisunidhi8@gmail.com>
2026-03-30 11:38:15 +0530

    Fix formatting issues in MAXIMUM PRODUCT SUBARRAY.cpp

--- MAXIMUM PRODUCT SUBARRAY.cpp
+++ MAXIMUM PRODUCT SUBARRAY.cpp
@@ -1,6 +1,6 @@
 //Given an array arr[] that contains positive and negative integers (may contain 0 as well). Find the maximum product that we can get in a subarray of arr[].
 //Note: It is guaranteed that the output fits in a 32-bit integer.
-
+ 
 class Solution {
   public:
     // Function to find maximum product subarray
