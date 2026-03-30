a058539289df5911a8201b0544b69b818d79a6a6
Sunidhi Chaturvedi <chaturvedisunidhi8@gmail.com>
2026-03-30 11:39:08 +0530

    Fix class declaration in sorted array pair solution

--- Pair with given sum in a sorted array.cpp
+++ Pair with given sum in a sorted array.cpp
@@ -1,7 +1,7 @@
 // You are given an integer target and an array arr[]. You have to find number of pairs in arr[] which sums up to target. It is given that the elements of the arr[] are in sorted order.
 // Note: pairs should have elements of distinct indexes. 
 
-class Solution {
+class Solution { 
   public:
     int countPairs(vector<int> &arr, int target) {
         // Complete the function
