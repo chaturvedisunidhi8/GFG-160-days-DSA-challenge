610fef297d3bbfb6a4f748355cd496daec3789e6
Sunidhi Chaturvedi <chaturvedisunidhi8@gmail.com>
2026-06-14 17:35:28 +0530

    Update comment on binary string addition

--- ADD TWO BINARY STRINGS.cpp
+++ ADD TWO BINARY STRINGS.cpp
@@ -1,5 +1,5 @@
 //Given two binary strings s1 and s2 consisting of only 0s and 1s. Find the resultant string after adding the two Binary Strings.
-//Note: The input strings may contain leading zeros but the output string should not have any leading zeros.
+//Note: The input strings may contain leading zeros but the output string should not have any leading zeros. 
 class Solution {  
   public: 
     string addBinary(string& s1, string& s2) { 
